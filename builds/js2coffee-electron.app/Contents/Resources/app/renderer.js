a8b53a84415dca857da7944dcef0dcd8b3950497
Igor Tkachenko <foozzione@gmail.com>
2016-11-14 19:56:21 -0800

    add build for mac os

---
+++ builds/js2coffee-electron.app/Contents/Resources/app/renderer.js
@@ -0,0 +1,21 @@
+(function(){
+    const {BrowserWindow} = require('electron').remote
+    function init() {
+        // Minimize task
+        document.getElementById("min-btn").addEventListener("click", (e) => {
+            var window = BrowserWindow.getFocusedWindow();
+            window.minimize();
+        });
+
+        // Close app
+        document.getElementById("close-btn").addEventListener("click", (e) => {
+            var window = BrowserWindow.getFocusedWindow();
+            window.close();
+        });
+    }
+    document.onreadystatechange =  () => {
+        if (document.readyState == "complete") {
+            init();
+        }
+    };
+})();
